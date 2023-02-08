4d5262e88cfdbac71bbee493702ffd4e5ea735d3
Jairth <rodriguez6191@hotmail.com>
2023-02-08 16:38:25 -0500

    update putProduct

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,18 +10,18 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   postProduct(data:any):Observable<any>{
-    return this.http.post<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList',data)
+    return this.http.post<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList/',data)
   }
 
   getProduct():Observable<any>{
-    return this.http.get<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList')
+    return this.http.get<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList/')
   }
 
-  putProduct(data:any,id:number):Observable<any> {
-    return this.http.put<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList'+id,data)
+  putProduct(data:any,id:string):Observable<any> {
+    return this.http.put<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList/'+id,data)
   }
 
   deleteProduct(id:number) {
-    return this.http.delete<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList'+id)
+    return this.http.delete<any>('https://63bc3614cf99234bfa73a9ce.mockapi.io/api/productList/'+id)
   }
 }
